b58aa2b776a134bb8e027d137624f06c560e7273
Diego Morales <77738746+Diego2250@users.noreply.github.com>
2023-07-14 19:22:12 -0600

    SDL Configuration

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <fstream>
+#include <SDL.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* argv[])
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window* window = SDL_CreateWindow("hola", 0, 0, 1200, 800, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+    SDL_SetRenderDrawColor(renderer, 100, 60, 60, 1);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(10000);
+
     return 0;
 }
